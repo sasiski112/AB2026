9b675585a70cc9301606b2cf84efbff0320e333e
sasiski112 <mshevelovv@gmail.com>
2026-03-26 15:53:14 +0200

    Add files via upload

---
+++ firma.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Loomise aeg: Märts 26, 2026 kell 02:52 PL
+-- Serveri versioon: 10.4.32-MariaDB
+-- PHP versioon: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Andmebaas: `matviiprac`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabeli struktuur tabelile `firma`
+--
+
+CREATE TABLE `firma` (
+  `firmaID` int(11) NOT NULL,
+  `firmanimi` varchar(30) DEFAULT NULL,
+  `aadress` varchar(30) DEFAULT NULL,
+  `telefon` varchar(13) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Andmete tõmmistamine tabelile `firma`
+--
+
+INSERT INTO `firma` (`firmaID`, `firmanimi`, `aadress`, `telefon`) VALUES
+(1, 'Maheevka', 'Tatarskaja 4', '0758231'),
+(2, 'Yashkino', 'Rimska 12', '88148244'),
+(3, 'McDonalds', 'Laagri tee 13', '78437993'),
+(4, 'KFC', 'Narva tee 56', '56741117'),
+(5, 'Hrust team', 'Pireskaya 3', '1237576'),
+(6, 'Casino Olimpic', 'Tallinn', '54555253'),
+(7, 'test', 'teAdress', '1111 1111');
+
+--
+-- Indeksid tõmmistatud tabelitele
+--
+
+--
+-- Indeksid tabelile `firma`
+--
+ALTER TABLE `firma`
+  ADD PRIMARY KEY (`firmaID`);
+
+--
+-- AUTO_INCREMENT tõmmistatud tabelitele
+--
+
+--
+-- AUTO_INCREMENT tabelile `firma`
+--
+ALTER TABLE `firma`
+  MODIFY `firmaID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
